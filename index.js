954ac9511dfeac017e62531ecaa40cd2a7ff9e22
nchore <nchoreanthony@gmail.com>
2023-09-20 20:46:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+num1 = 31
+num2 = 2
+multiply = num1 * num2
+console.log(multiply);
 
-// Write your code here
+// Questio 2: Creating random numbers greater than 0 
+
+Math.floor(Math.random() * 10) + 1;
+
+// Question 3
+
+const num3 = 22
+const num4 = 9
+mod = (num3 / num4)
+console.log(mod);
+
+// Question 4
+
+Math.max(1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
